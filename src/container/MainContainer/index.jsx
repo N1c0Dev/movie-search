12975cc3b4476de5d8700212b4dbf6db4837ea4f
Nicodev <nicolasgongar@gmail.com>
2020-04-22 00:15:36 -0400

    Added default states

--- src/container/MainContainer/index.jsx
+++ src/container/MainContainer/index.jsx
@@ -3,7 +3,13 @@ import {
   Container,
   Row,
   Navbar,
+  Col,
 } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFilm,
+  faHeart,
+} from '@fortawesome/free-solid-svg-icons'
 import MovieContainer from '../MovieContainer'
 import './styles.css'
 
@@ -12,6 +18,9 @@ export default class MainContainer extends React.Component {
   state = {
     movieSearch: '',
     movieResults: [],
+    noMovieText: "No movies found",
+    noFavouriteText: "No favourites found",
+    searchPlaceholder: "Type the movie title and press Enter..."
   }
 
   handleMovieSearch = ({target}) => {
@@ -25,7 +34,7 @@ export default class MainContainer extends React.Component {
     .then(resp => resp.json())
     .then(result => {
       this.setState({
-        movieResults: result.Search,
+        movieResults: result.hasOwnProperty('Search') ? result.Search : [],
       })
     })
   }
@@ -39,6 +48,9 @@ export default class MainContainer extends React.Component {
   render() {
     const {
       movieResults,
+      noMovieText,
+      noFavouriteText,
+      searchPlaceholder,
     } = this.state
 
     return (
@@ -46,20 +58,39 @@ export default class MainContainer extends React.Component {
         <Navbar className="navbar-header">
           <Container>
             <Row className="width-100">
-              <input className="width-30" type="text" placeholder="Type the movie name and press Enter..." onChange={this.handleMovieSearch} onKeyPress={this.handleEnterSearch} />
+              <input className="width-30" type="text" placeholder={searchPlaceholder} onChange={this.handleMovieSearch} onKeyPress={this.handleEnterSearch} />
             </Row>
           </Container>
         </Navbar>
         <Container>
           <div className="title">Movie List</div>
-          <Row className="list-container">
-            <MovieContainer movies={movieResults} />
-          </Row>
+          {
+            movieResults.length <= 0 ? (
+              <Row className="list-container">
+                <Col className="text-center image-container" xs={12} sm={12} md={4} lg={4} />
+                <Col className="text-center image-container" xs={12} sm={12} md={4} lg={4} >
+                  <FontAwesomeIcon className="default-icons" icon={faFilm}/>
+                  <div className="default-title">{noMovieText}</div>
+                </Col>
+              <Col className="text-center image-container" xs={12} sm={12} md={4} lg={4} />
+              </Row>
+            ) : (
+              <Row className="list-container">
+                <MovieContainer movies={movieResults} />
+              </Row>
+            )
+          }
+
         </Container>
         <Container>
           <div className="title">Favorites</div>
-          <Row className="list-container">
-            <MovieContainer movies={movieResults} />
+          <Row className="list-container margin-bottom-50">
+            <Col className="text-center image-container" xs={12} sm={12} md={4} lg={4} />
+            <Col className="text-center image-container" xs={12} sm={12} md={4} lg={4}>
+              <FontAwesomeIcon className="default-icons" icon={faHeart} />
+              <div className="default-title">{noFavouriteText}</div>
+            </Col>
+            <Col className="text-center image-container" xs={12} sm={12} md={4} lg={4} />
           </Row>
         </Container>
       </div>
